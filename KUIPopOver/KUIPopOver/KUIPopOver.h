9398cdeaa2a4705a3091c214d586434826216485
kof.kofktu <kof.kofktu@kakaocorp.com>
2017-08-31 15:36:43 +0900

    Start Xcode project

---
+++ KUIPopOver/KUIPopOver/KUIPopOver.h
@@ -0,0 +1,19 @@
+//
+//  KUIPopOver.h
+//  KUIPopOver
+//
+//  Created by kofktu on 2017. 8. 31..
+//  Copyright © 2017년 Kofktu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KUIPopOver.
+FOUNDATION_EXPORT double KUIPopOverVersionNumber;
+
+//! Project version string for KUIPopOver.
+FOUNDATION_EXPORT const unsigned char KUIPopOverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KUIPopOver/PublicHeader.h>
+
+
