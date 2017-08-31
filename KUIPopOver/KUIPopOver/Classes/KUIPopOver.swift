e48f2874a3ab52caa3422459979a16fcff2dbdb0
kof.kofktu <kof.kofktu@kakaocorp.com>
2017-08-31 15:45:27 +0900

    add KUIPopOver, KUIPopOverUsable.swift

---
+++ KUIPopOver/KUIPopOver/Classes/KUIPopOver.swift
@@ -0,0 +1,11 @@
+//
+//  KUIPopOver.swift
+//  KUIPopOver
+//
+//  Created by kofktu on 2017. 8. 31..
+//  Copyright © 2017년 Kofktu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
---
+++ KUIPopOver/KUIPopOver/Classes/KUIPopOverUsable.swift
@@ -0,0 +1,17 @@
+//
+//  KUIPopOverUsable.swift
+//  KUIPopOver
+//
+//  Created by kofktu on 2017. 8. 31..
+//  Copyright © 2017년 Kofktu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol KUIPopOverUsable {
+    
+    var preferredContentSize: CGSize { get }
+    var contentView: UIView { get }
+    
+}
